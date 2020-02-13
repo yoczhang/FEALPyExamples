a3a125bb8c95bfd496b4b313f003ffc656c9c1c1
yczhang <yoczhang@126.com>
2020-02-13 22:47:22 +0800

    up

--- myExamples/Test/totest_WGSpace.py
+++ myExamples/Test/totest_WGSpace.py
@@ -11,6 +11,7 @@
 
 
 import numpy as np
+from numpy.linalg import inv
 import matplotlib.pyplot as plt
 from fealpy.mesh.TriangleMesh import TriangleMesh
 from fealpy.mesh.Quadtree import Quadtree
@@ -91,42 +92,69 @@ n = pmesh.edge_unit_normal()
 qf = GaussLegendreQuadrature(p + 3)
 bcs, ws = qf.quadpts, qf.weights
 ps = np.einsum('ij, kjm->ikm', bcs, node[edge])
-phi0 = smspace.basis(ps, index=edge2cell[:, 0])
-phi1 = smspace.basis(ps[:, isInEdge, :], index=edge2cell[isInEdge, 1])
-phi = smspace.edge_basis(ps)
+phi0 = smspace.basis(ps, index=edge2cell[:, 0])  # (NQ,NE,smldof)
+phi1 = smspace.basis(ps[:, isInEdge, :], index=edge2cell[isInEdge, 1])  # (NQ,NInE,smldof)
+phi = smspace.edge_basis(ps)  # (NQ,NE,ldof1d)
 
-F0 = np.einsum('i, ijm, ijn, j->mjn', ws, phi0, phi, h)
+F0 = np.einsum('i, ijm, ijn, j->mjn', ws, phi0, phi, h)  # (smldof,NE,ldof1d)
 F1 = np.einsum('i, ijm, ijn, j->mjn', ws, phi1, phi[:, isInEdge, :], h[isInEdge])
 
 smldof = smspace.number_of_local_dofs()
 cell2dof, cell2dofLocation = wgdof.cell2dof, wgdof.cell2dofLocation
 R0 = np.zeros((smldof, len(cell2dof)), dtype=np.float)
+# # R0.shape: (smldof,NC*CNldof), CNldof is the number of all dofs in one cell
 R1 = np.zeros((smldof, len(cell2dof)), dtype=np.float)
 
-idx = cell2dofLocation[edge2cell[:, [0]]] + edge2cell[:, [2]] * (p + 1) + np.arange(p + 1)
+idx = cell2dofLocation[edge2cell[:, [0]]] + edge2cell[:, [2]] * (p + 1) + np.arange(p + 1)  # (NE,ldof1d)
 R0[:, idx] = n[np.newaxis, :, [0]] * F0
 R1[:, idx] = n[np.newaxis, :, [1]] * F0
 if isInEdge.sum() > 0:
     idx1 = cell2dofLocation[edge2cell[isInEdge, 1]].reshape(-1, 1) \
-          + (p + 1) * edge2cell[isInEdge, [3]].reshape(-1, 1) + np.arange(p + 1)
+           + (p + 1) * edge2cell[isInEdge, [3]].reshape(-1, 1) + np.arange(p + 1)
     n = n[isInEdge]
     R0[:, idx1] = -n[np.newaxis, :, [0]] * F1  # 这里应该加上负号
     R1[:, idx1] = -n[np.newaxis, :, [1]] * F1  # 这里应该加上负号
 
 
 def f(x, index):
-    gphi = smspace.grad_basis(x, index)
-    phi = smspace.basis(x, index)
+    gphi = smspace.grad_basis(x, index)  # (NQ,NInE,smldof,2)
+    phi = smspace.basis(x, index)  # (NQ,NInE,smldof)
+    # the return: (2,smldof,smldof)
     return np.einsum('...mn, ...k->...nmk', gphi, phi)
 
 
-M = integralalg.integral(f, celltype=True)
+M = integralalg.integral(f, celltype=True)  # (NC,2,smldof,smldof)
 idof = (p + 1) * (p + 2) // 2
-idx = cell2dofLocation[1:].reshape(-1, 1) + np.arange(-idof, 0)
-R0[:, idx] = -M[:, 0].swapaxes(0, 1)
+idx = cell2dofLocation[1:].reshape(-1, 1) + np.arange(-idof, 0)  # (NC,smldof)
+R0[:, idx] = -M[:, 0].swapaxes(0, 1)  # M[:, 0].shape (NC,smldof,smldof)
 R1[:, idx] = -M[:, 1].swapaxes(0, 1)
 
 tR0 = np.hsplit(R0, cell2dofLocation[1:-1])
+
+# --- weak gradient --- #
+CM = smspace.cell_mass_matrix()
+EM = smspace.edge_mass_matrix()
+
+H0 = inv(CM)
+H1 = inv(EM)
+
+cd = np.hsplit(cell2dof, cell2dofLocation[1:-1])
+r0 = np.hsplit(R0, cell2dofLocation[1:-1])
+r1 = np.hsplit(R1, cell2dofLocation[1:-1])
+ph = smspace.function(dim=2)
+
+uh = np.ones(len(cell2dof))
+f0 = lambda x: x[0] @ (x[1] @ uh[x[2]])
+ph[:, 0] = np.concatenate(list(map(f0, zip(H0, r0, cd))))
+# # Here,
+# # H0 is array: (NC,smldof,smldof);
+# # r0 is list: len(r0)=4, r0[0].shape=(smldof,CNdof), CNldof is the number of all dofs in one cell
+# # cd is list: len(cd)=4, cd[0].shape=(CNdof,)
+# # Every-time the x of f0 is x=(H0[i,...], r0[i], cd[i]), i = 0,1,2,3,
+# # i.e., x[0] is H0[i,...]; x[1] is r0[i]; x[2] is cd[i].
+
+
+ph[:, 1] = np.concatenate(list(map(f0, zip(H0, r1, cd))))
 
 # ------------------------------------------------- #
 print("End of this test file")
