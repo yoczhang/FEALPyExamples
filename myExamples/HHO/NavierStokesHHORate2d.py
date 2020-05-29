584872a0001dca5e37be8ed41fca83a025d7b068
yczhang <yoczhang@126.com>
2020-05-29 22:37:15 +0800

    Create NavierStokesHHORate2d.py

---
+++ myExamples/HHO/NavierStokesHHORate2d.py
@@ -0,0 +1,11 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @Site: 
+# @File: NavierStokesHHORate2d.py
+# @Author: Yongchao Zhang
+# @E-mail: yoczhang@126.com
+# @Time: May 29, 2020
+# ---
+
