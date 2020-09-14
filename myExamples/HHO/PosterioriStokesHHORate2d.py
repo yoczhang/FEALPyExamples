4b980c9d8dbfda53e3c23d048941318c237a445a
YcZhang <yoczhang@126.com>
2020-09-14 14:19:35 +0800

    update

---
+++ myExamples/HHO/PosterioriStokesHHORate2d.py
@@ -0,0 +1,15 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: PosterioriStokesHHORate2d.py
+# @Author: Yongchao Zhang, Northwest University
+# @E-mail: yoczhang@nwu.edu.cn
+# @Site:
+# @Time: Sep 14, 2020
+# ---
+
+__doc__ = """
+The fealpy program for posteriori Stokes problem. 
+"""
+
