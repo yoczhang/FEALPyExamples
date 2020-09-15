5c215fa39983c57e760bc2a9b8524fe5d5bade8d
YcZhang <yoczhang@126.com>
2020-09-15 15:35:25 +0800

    update

---
+++ myExamples/Test/totest_HHOStokesSpace.py
@@ -0,0 +1,11 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: totest_HHOStokesSpace.py
+# @Author: Yongchao Zhang, Northwest University, Xi'an
+# @E-mail: yoczhang@nwu.edu.cn
+# @Site:
+# @Time: Sep 15, 2020
+# ---
+
