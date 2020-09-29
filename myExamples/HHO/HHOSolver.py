75132d3b1720a4f6df9c37b2b02d52dbf5db4a73
YcZhang <yoczhang@126.com>
2020-09-29 20:33:26 +0800

    update

--- myExamples/HHO/HHOSolver.py
+++ myExamples/HHO/HHOSolver.py
@@ -18,8 +18,8 @@ from scipy.sparse.linalg import spsolve
 
 
 class HHOSolver:
-    def __init__(self, A, R, space):
-        self.A = A
+    def __init__(self, M, R, space):
+        self.M = M
         self.R = R
         self.space = space
         self.p = self.space.p
@@ -28,18 +28,36 @@ class HHOSolver:
         self.NE = self.mesh.number_of_edges()
 
     def StokesSolver(self):
-        A = self.A
+        M = self.M
         R = self.R
         p = self.p
         mesh = self.mesh
         NC = self.NC
         NE = self.NE
 
-        gNdof_uT = NC * (p + 1) * (p + 2) // 2
-        gNdof_uF = NE * (p + 1)
-        gNdof_pT = NC * (p + 1) * (p + 2) // 2
+        uTgNdof = NC * (p + 1) * (p + 2) // 2
+        uFgNdof = NE * (p + 1)
+        ugNdof = uTgNdof + uFgNdof
+        pTgNdof = NC * (p + 1) * (p + 2) // 2
 
+        MA1_00 = M[:uTgNdof, :uTgNdof]
+        MA1_0b = M[:uTgNdof, uTgNdof:ugNdof]
+        MA1_b0 = M[uTgNdof:ugNdof, :uTgNdof]
+        MA1_bb = M[uTgNdof:ugNdof, uTgNdof:ugNdof]
+
+        MA2_00 = np.copy(MA1_00)
+        MA2_0b = np.copy(MA1_0b)
+        MA2_b0 = np.copy(MA1_b0)
+        MA2_bb = np.copy(MA1_bb)
+
+        MB1_0 = M[:uTgNdof, 2*ugNdof:2*ugNdof + pTgNdof]
+        MB1_b = M[uTgNdof:ugNdof, 2*ugNdof:2*ugNdof + pTgNdof]
         
+
+
+
+
+
 
 
 
