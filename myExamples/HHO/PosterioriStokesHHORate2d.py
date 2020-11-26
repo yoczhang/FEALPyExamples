e354b328594d0f97a00c1a42202f474d744e3b68
YcZhang <yoczhang@126.com>
2020-11-26 14:37:25 +0800

    update

--- myExamples/HHO/PosterioriStokesHHORate2d.py
+++ myExamples/HHO/PosterioriStokesHHORate2d.py
@@ -88,13 +88,20 @@ sc = ShowCls(p, mesh, errorType=errorType, Ndof=Ndof, errorMatrix=errorMatrix, o
 # --- start for-loop --- #
 stokes = None
 sol = None
-tol = 1.0e-2
+tol = 5.0e-1
 print('nu = %e' % nu)
-for i in range(maxit):  # range(maxit), [maxit-1]
+i = 0
+ETA = 1.0
+# for i in range(maxit):  # range(maxit), [maxit-1]
+while ETA > tol:
     print('\n# --------------------- i = %d ------------------------- #' % i)
     stokes = StokesHHOModel2d(pde, mesh, p)
     sol = stokes.solve()
     uh = sol['uh']
+
+    # --- expand Ndof and errorMatrix --- #
+
+
     Ndof[i] = stokes.space.number_of_velocity_dofs()  # get the number of dofs
     errorMatrix[0, i] = (nu ** 0.5) * np.sqrt(np.sum(stokes.velocity_L2_error(celltype=True)**2))  # get the velocity L2 error
 
@@ -105,32 +112,34 @@ for i in range(maxit):  # range(maxit), [maxit-1]
 
     # --- adaptive settings --- #
     eta = stokes.space.residual_estimate0(nu, uh, pde.source, pde.velocity)  # (NC,)
-    errorMatrix[4, i] = np.sqrt(np.sum(eta**2))
-    eff0 = 1./np.sqrt(np.sum(eta**2) / (errorMatrix[1, i]**2 + errorMatrix[3, i]**2))
-    eff1 = 1. / np.sqrt(np.sum(eta**2) / (errorMatrix[2, i] ** 2 + errorMatrix[3, i] ** 2))
+    ETA = np.sqrt(np.sum(eta ** 2))
+    errorMatrix[4, i] = ETA
+    eff0 = 1./np.sqrt(ETA / (errorMatrix[1, i]**2 + errorMatrix[3, i]**2))
+    eff1 = 1. / np.sqrt(ETA / (errorMatrix[2, i] ** 2 + errorMatrix[3, i] ** 2))
     print('Posteriori Info:')
-    print('  |___ eff0 = %f, eff1 = %f: ' % (eff0, eff1))
+    print('  |___ ETA = %e, eff0 = %f, eff1 = %f: ' % (ETA, eff0, eff1))
     print('  |___ before refine: number of cells: %d, pressure dofs: %d ' % (
         mesh.number_of_cells(), stokes.space.number_of_pressure_dofs()))
 
-    sc.showMesh(markCell=False, markEdge=False, markNode=False)
-    fig1 = plt.figure()
-    axes = fig1.gca()
-    mesh.add_plot(axes)
-    outPath_1 = outPath + str(i) + '-mesh.png'
-    plt.savefig(outPath_1)
-    plt.close()
+    # sc.showMesh(markCell=False, markEdge=False, markNode=False)
+    # fig1 = plt.figure()
+    # axes = fig1.gca()
+    # mesh.add_plot(axes)
+    # outPath_1 = outPath + str(i) + '-mesh.png'
+    # plt.savefig(outPath_1)
+    # plt.close()
 
     # --- adaptive refine the mesh --- #
-    aopts = mesh.adaptive_options(method='max', theta=0.3, maxcoarsen=0.1, HB=True)
-    mesh.adaptive(eta, aopts) if i < maxit - 1 else None
+    # aopts = mesh.adaptive_options(method='max', theta=0.3, maxcoarsen=0.1, HB=True)
+    # mesh.adaptive(eta, aopts) if i < maxit - 1 else None
 
-    isMarkedCell = stokes.space.post_estimator_markcell(eta, theta=0.3)
-    mesh.refine_poly(isMarkedCell) if i < maxit - 1 else None
+    isMarkedCell = stokes.space.post_estimator_markcell(eta, theta=0.4)
+    mesh.refine_poly(isMarkedCell) if (i < maxit - 1) & (ETA > tol) else None
 
     # --- uniform refine the mesh --- #
     # mesh.uniform_refine() if i < maxit - 1 else None
     print('  |___ after refine: number of cells: ', mesh.number_of_cells())
+    i += 1
 
 # --- plot solution --- #
 stokes.showSolution(sc)
