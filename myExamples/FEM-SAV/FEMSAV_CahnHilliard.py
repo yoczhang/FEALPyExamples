5262fc2651066a63e60a9fa082c1e00f155cbd3e
YcZhang <yoczhang@126.com>
2021-05-31 09:29:26 +0800

    update

---
+++ myExamples/FEM-SAV/FEMSAV_CahnHilliard.py
@@ -0,0 +1,16 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: FEMSAV_CahnHilliard.py
+# @Author: Yongchao Zhang, Northwest University, Xi'an
+# @E-mail: yoczhang@nwu.edu.cn
+# @Site:
+# @Time: May 31, 2021
+# ---
+
+__doc__ = """
+The fealpy-FEM program for Cahn-Hilliard problem. 
+"""
+
+import numpy as np
---
+++ myExamples/FEM-SAV/FEMSAV_NavierStokes.py
@@ -0,0 +1,16 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: FEMSAV_NavierStokes.py
+# @Author: Yongchao Zhang, Northwest University, Xi'an
+# @E-mail: yoczhang@nwu.edu.cn
+# @Site:
+# @Time: May 31, 2021
+# ---
+
+__doc__ = """
+The fealpy-FEM program for Navier-Stokes problem. 
+"""
+
+import numpy as np
