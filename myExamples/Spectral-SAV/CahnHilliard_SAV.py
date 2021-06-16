e57a4617caa15d77cd845170d8db7b5c64b42a6a
YcZhang <yoczhang@126.com>
2021-06-16 22:20:03 +0800

    update

---
+++ myExamples/Spectral-SAV/CahnHilliard_SAV.py
@@ -0,0 +1,15 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: CahnHilliard_SAV.py
+# @Author: Yongchao Zhang, Northwest University, Xi'an
+# @E-mail: yoczhang@nwu.edu.cn
+# @Site:
+# @Time: Jun 16, 2021
+# ---
+
+__doc__ = """
+The Fourier spectral methods for Cahn-Hilliard equation by SAV.
+The refer paper: 2019 (SIAM ShenJie) A New Class of Efficient and Robust Energy Stable Schemes for Gradient Flows
+"""
