fa69af966815dc807d4b9dff8406bb496644cb98
YcZhang <yoczhang@126.com>
2021-08-10 22:30:09 +0800

    update

--- myExamples/FEM_CH_VCNS/FEM_CH_NS_Model2d.py
+++ myExamples/FEM_CH_VCNS/FEM_CH_NS_Model2d.py
@@ -99,6 +99,79 @@ class FEM_CH_NS_Model2d:
         guh_val[..., 1] = 3 * uh_val ** 2 * guh_val[..., 1] - guh_val[..., 1]
         return guh_val  # (NQ,NC,2)
 
+    def CH_NS_Solver_T1stOrder(self):
+        pde = self.pde
+        timemesh = self.timemesh
+        NT = len(timemesh)
+        dt = self.dt
+        uh = self.uh
+        wh = self.wh
+        sm = self.StiffMatrix
+        mm = self.MassMatrix
+        space = self.space
+        dof = self.dof
+        face2dof = dof.face_to_dof()  # (NE,fldof)
+        cell2dof = dof.cell_to_dof()  # (NC,cldof)
+
+        dt_min = pde.dt_min if hasattr(pde, 'dt_min') else dt
+        s, alpha = self.setCoefficient_T1stOrder(dt_minimum=dt_min)
+        m = pde.m
+        epsilon = pde.epsilon
+        eta = pde.eta
+
+        print('    # #################################### #')
+        print('      Time 1st-order scheme')
+        print('    # #################################### #')
+
+        print('    # ------------ parameters ------------ #')
+        print('    s = %.4e,  alpha = %.4e,  m = %.4e,  epsilon = %.4e,  eta = %.4e' % (s, alpha, m, epsilon, eta))
+        print('    t0 = %.4e,  T = %.4e, dt = %.4e' % (timemesh[0], timemesh[-1], dt))
+        print(' ')
+
+        idxNeuEdge_CH = self.set_CH_Neumann_edge()
+        n_Bd_CH = self.mesh.face_unit_normal(index=idxNeuEdge_CH)  # (NBE,2)
+        NeuCellIdx_CH = self.mesh.ds.edge2cell[idxNeuEdge_CH, 0]
+        NeuLocalIdx_CH = self.mesh.ds.edge2cell[idxNeuEdge_CH, 2]
+        Neu_face_measure_CH = self.mesh.entity_measure('face', index=idxNeuEdge_CH)  # (Nneu,2)
+        cell_measure = self.mesh.cell_area()
+
+        f_q = self.integralalg.faceintegrator
+        f_bcs, f_ws = f_q.get_quadrature_points_and_weights()  # f_bcs.shape: (NQ,(GD-1)+1)
+        f_pp_Neu_CH = self.mesh.bc_to_point(f_bcs, index=idxNeuEdge_CH)  # f_pp.shape: (NQ,NBE,GD) the physical Gauss points
+        c_q = self.integralalg.cellintegrator
+        c_bcs, c_ws = c_q.get_quadrature_points_and_weights()  # c_bcs.shape: (NQ,GD+1)
+        c_pp = self.mesh.bc_to_point(c_bcs)  # c_pp.shape: (NQ_cell,NC,GD) the physical Gauss points
+
+        phi_f_Neu_CH = space.face_basis(f_bcs)  # (NQ,1,fldof). 实际上这里可以直接用 pspace.basis(f_bcs), 两个函数的代码是相同的
+        phi_c = space.basis(c_bcs)  # (NQ,NC,clodf)
+        gphi_c = space.grad_basis(c_bcs)  # (NQ,NC,cldof,GD)
+
+
+    def set_NS_Dirichlet_edge(self, idxDirEdge=None):
+        if idxDirEdge is not None:
+            return idxDirEdge
+
+        mesh = self.mesh
+        edge2cell = mesh.ds.edge_to_cell()
+        isBdEdge = (edge2cell[:, 0] == edge2cell[:, 1])  # (NE,), the bool vars, to get the boundary edges
+
+        isDirEdge = isBdEdge  # here, we set all the boundary edges are Dir edges
+        idxDirEdge, = np.nonzero(isDirEdge)  # (NE_Dir,)
+
+        return idxDirEdge
+
+    def set_CH_Neumann_edge(self, idxNeuEdge=None):
+        if idxNeuEdge is not None:
+            return idxNeuEdge
+
+        mesh = self.mesh
+        edge2cell = mesh.ds.edge_to_cell()
+        bdEdge = (edge2cell[:, 0] == edge2cell[:, 1])  # the bool vars, to get the boundary edges
+
+        isNeuEdge = bdEdge  # here, we first set all the boundary edges are Neu edges
+        idxNeuEdge, = np.nonzero(isNeuEdge)  # (NE_Dir,)
+        return idxNeuEdge
+
 
 
 
