85abe38f415e2fcf64969bf5287a4e62ea4c85be
YcZhang <yoczhang@126.com>
2021-12-20 18:10:34 +0800

    update

--- myExamples/FEM_CH_NS_VarCoeff/symCompute_CHNS_VarCoeff.py
+++ myExamples/FEM_CH_NS_VarCoeff/symCompute_CHNS_VarCoeff.py
@@ -14,8 +14,9 @@
 from sympy import symbols, cos, sin, diff, exp
 import numpy as np
 
-t, x, y, nu, pi = symbols('t x y nu pi')
+t, x, y, pi = symbols('t x y pi')
 epsilon, eta, m = symbols('epsilon eta m')
+rho0, rho1, nu0, nu1 = symbols('rho0 rho1 nu0 nu1')
 
 # # ------------------- ex0 ------------------- # #
 # # the CH equation:
@@ -40,8 +41,14 @@ c_yy = diff(c_y, y)
 laplace_c = c_xx + c_yy
 
 # # the NS equation:
-vel0 = x * x * y * exp(-t)
-vel1 = -x * y * y * exp(-t)
+# pre settings:
+J_0 = -(rho0 - rho1)/2 * m * c_x
+J_1 = -(rho0 - rho1)/2 * m * c_y
+rho = (rho0 + rho1)/2 + (rho0 - rho1)/2 * u
+nu = (nu0 + nu1)/2 + (nu0 - nu1)/2 * u
+
+vel0 = sin(pi*x)*cos(pi*y)*sin(t)
+vel1 = -cos(pi*x)*sin(pi*y)*sin(t)
 
 vel0_t = diff(vel0, t)
 vel0_x = diff(vel0, x)
@@ -55,7 +62,12 @@ vel1_xx = diff(vel1_x, x)
 vel1_y = diff(vel1, y)
 vel1_yy = diff(vel1_y, y)
 
-p = (x * y - 1 / 4) * exp(-t)
+vel_stress00 = vel0_x
+vel_stress01 = 0.5*(vel0_y + vel1_x)
+vel_stress10 = 0.5*(vel0_y + vel1_x)
+vel_stress11 = vel1_y
+
+p = sin(pi*x)*sin(pi*y)*cos(t)
 p_x = diff(p, x)
 p_y = diff(p, y)
 
@@ -63,8 +75,8 @@ p_y = diff(p, y)
 g_CH = u_t - m*laplace_c + diff(u*vel0, x) + diff(u*vel1, y)
 
 # # 2. coupled NS: source term
-g_NS_0 = vel0_t - nu * (vel0_xx + vel0_yy) + (vel0*vel0_x + vel1*vel0_y) + p_x + u * c_x
-g_NS_1 = vel1_t - nu * (vel1_xx + vel1_yy) + (vel0*vel1_x + vel1*vel1_y) + p_y + u * c_y
+g_NS_0 = rho * vel0_t - (diff(nu*vel_stress00, x) + diff(nu*vel_stress01, y)) + rho * (vel0*vel0_x + vel1*vel0_y) + p_x + u * c_x
+g_NS_1 = rho * vel1_t - (diff(nu*vel_stress10, x) + diff(nu*vel_stress11, y)) + rho * (vel0*vel1_x + vel1*vel1_y) + p_y + u * c_y
 
 print('u = ', u)
 print('laplace_x = ', laplace_x)
