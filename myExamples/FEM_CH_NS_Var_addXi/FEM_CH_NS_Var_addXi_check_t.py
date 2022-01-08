2cac4b7d185fcfffecde7d109fa50de5df7cb357
yczhang <yoczhang@126.com>
2022-01-08 22:41:32 +0800

    update

--- myExamples/FEM_CH_NS_Var_addXi/FEM_CH_NS_Var_addXi_check_t.py
+++ myExamples/FEM_CH_NS_Var_addXi/FEM_CH_NS_Var_addXi_check_t.py
@@ -2,17 +2,30 @@
 # -*- coding: utf-8 -*-
 # ---
 # @Software: PyCharm
-# @File: FEM_CH_NS_VarCoeff_check_t.py
-# @Author: Yongchao Zhang, Northwest University, Xi'an
-# @E-mail: yoczhang@nwu.edu.cn
-# @Site:
-# @Time: Dec 13, 2021
+# @File: FEM_CH_NS_Var_addXi_check_t.py
+# @Author: Yongchao Zhang
+# @Institution: Northwest University, Xi'an, Shaanxi, China
+# @E-mail: yoczhang@126.com, yoczhang@nwu.edu.cn
+# @Site: 
+# @Time: Jan 08, 2022
 # ---
 
 
 __doc__ = """
-The fealpy-FEM program for Variable-Coefficient coupled Cahn-Hilliard-Navier-Stokes equation.
+The fealpy-FEM program for Variable-Coefficient coupled Cahn-Hilliard-Navier-Stokes equation, 
+add the solver for \\xi.
 """
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from CH_NS_VarCoeff_Data import CH_NS_VarCoeff_truesolution
+from FEM_CH_NS_VarCoeff_Model2d import FEM_CH_NS_VarCoeff_Model2d
+from fealpy.mesh import MeshFactory as MF
+from PrintLogger import make_print_to_file
+# from fealpy.tools.show import showmultirate, show_error_table
+from to_show import show_error_table
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -44,7 +57,7 @@ dt_space = 1e-1 * np.logspace(start, stop, N_T, base=1/2)
 dt_min = min(dt_space)
 
 time_scheme = 1  # 1 stands for 1st-order time-scheme; 2 is the 2nd-order time-scheme
-h_space = dt_space ** (time_scheme/(p+0))
+# # h_space = dt_space ** (time_scheme/(p+0))
 
 pdePars = {'m': 1e-3, 's': 1, 'alpha': 1, 'epsilon': 1e-3, 'eta': 1e-1, 'dt_min': dt_min, 'timeScheme': '1stOrder'
            }  # value of parameters
@@ -54,13 +67,16 @@ pde.setPDEParameters(pdePars)
 pde.setPDEParameters(VarCoeff)
 
 # # print some basic info
-print('\n# ------------ the initial parameters ------------ #')
+print('\n# ------------ in FEM_CH_NS_Var_addXi_check_t code ------------ #')
+print('# ------------ the initial parameters ------------ #')
 print('p = ', p)
-print('t0 = %.4e' % t0)
+print('t0 = %.4e,  T = %.4e' % (t0, T))
 print('dt_space = ', dt_space)
-print('h_space = ', h_space)
 print('domain box = ', box)
+print('rho0 = %.4e,  rho1 = %.4e' % (VarCoeff['rho0'], VarCoeff['rho1']))
+print('nu0 = %.4e,  nu1 = %.4e, ' % (VarCoeff['nu0'], VarCoeff['nu1']))
 print('# #')
+
 
 # # --- error settings
 errorType = ['$|| u - u_h||_0$', '$||\\nabla u - \\nabla u_h||_0$',
@@ -69,34 +85,9 @@ errorMatrix = np.zeros((len(errorType), N_T), dtype=np.float)
 Ndof = np.zeros(N_T, dtype=np.int)  # the array to store the number of dofs
 
 # --- start for-loop --- #
-box = pde.box if hasattr(pde, 'box') else box
-for i in range(N_T):
-    print('\n# *********************************************************************** # \n')
-    print('# ------------ in the time-mesh circle ------------ #')
-    print('i = ', i)
-    print('# -------------------------------------------------- #')
-    # NN = int(1./h_space[i]) + 1
-    NN = 32
-    mesh = MF.boxmesh2d(box, nx=NN, ny=NN, meshtype='tri')
-    if time_scheme == 1:
-        ch = FEM_CH_NS_VarCoeff_Model2d(pde, mesh, p, dt_space[i])
-        uh_l2err, uh_h1err, vel_l2err, vel_h1err, ph_l2err = ch.CH_NS_Solver_T1stOrder()
-    else:
-        raise ValueError("There has no other time-scheme")
+if hasattr(pde, 'box'):
+    box = pde.box
+    print('PDE has new domain box = ', box)
 
-    Ndof[i] = ch.number_of_global_dofs()
-    errorMatrix[0, i] = uh_l2err
-    errorMatrix[1, i] = uh_h1err
-    errorMatrix[2, i] = vel_l2err
-    errorMatrix[3, i] = vel_h1err
-    errorMatrix[4, i] = ph_l2err
 
-# --- get the convergence rate --- #
-print('# ------------ the error-table ------------ #')
-show_error_table(dt_space, errorType, errorMatrix, table_scheme='dt')
 
-# # plot the rate
-# showmultirate(plt, 0, Ndof, errorMatrix, errorType)
-# plt.show()
-
-print('# ------------ end of the file ------------ #')
