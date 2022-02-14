ac48389696cc8a63d0f8dbf64ea775635feb8957
yczhang <yoczhang@126.com>
2022-02-14 16:36:50 +0800

    upate

---
+++ myExamples/__init__.py
@@ -0,0 +1,11 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: __init__.py
+# @Author: Yongchao Zhang
+# @Institution: Northwest University, Xi'an, Shaanxi, China
+# @E-mail: yoczhang@126.com, yoczhang@nwu.edu.cn
+# @Site: 
+# @Time: Feb 14, 2022
+# ---
