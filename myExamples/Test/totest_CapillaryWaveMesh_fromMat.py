1de3be5e141546eec573d2652553ac99086c7e56
YcZhang <yoczhang@126.com>
2022-02-15 22:17:35 +0800

    update

--- myExamples/Test/totest_CapillaryWaveMesh_fromMat.py
+++ myExamples/Test/totest_CapillaryWaveMesh_fromMat.py
@@ -14,12 +14,43 @@ import numpy as np
 from scipy.io import loadmat
 from fealpy.mesh.TriangleMesh import TriangleMesh
 from fealpy.mesh.HalfEdgeMesh2d import HalfEdgeMesh2d
+import matplotlib  # 为了解决画图时采用 GUI (plt.show()) 的形式时, python3.8 崩溃的情况.
+matplotlib.use("TkAgg")  # 为了解决画图时采用 GUI (plt.show()) 的形式时, python3.8 崩溃的情况.
+import matplotlib.pyplot as plt
+import os, sys
+os.chdir(sys.path[0])
 
-
-mfile = loadmat('CapillaryWaveMesh.mat')
+print(os.getcwd())
+mfile = loadmat('./CapillaryWaveMesh.mat')
 node = mfile['node']
 cell = mfile['elem']
 mesh = TriangleMesh(node, cell)
 mesh = HalfEdgeMesh2d.from_mesh(mesh, NV=3)
 
+Nrefine = 2
+for k in range(Nrefine):
+    bc = mesh.cell_barycenter()
+    NC = mesh.number_of_cells()
+    cellstart = mesh.ds.cellstart
+    isMarkedCell = np.zeros(NC + cellstart, dtype=np.bool_)
+    isMarkedCell[cellstart:] = abs(bc[:, 1] - 0.) < 0.015
+    # mesh.refine_triangle_rg(isMarkedCell)
+    mesh.refine_triangle_nvb(isMarkedCell)
+    bc1 = mesh.cell_barycenter()
+    NC1 = mesh.number_of_cells() 
+    cc, = np.nonzero(abs(bc[:, 1] - 0.) < 0.015)
+
+node = mesh.node
+cell = mesh.ds.cell_to_node()
+np.save('./WaveMeshNode_mat', node)
+np.save('./WaveMeshCell_mat', cell)
+
+# fig = plt.figure()
+# axes = fig.gca()
+# mesh.add_plot(axes)
+# # mesh.find_cell(axes, showindex=True)
+# plt.show()
+# plt.close()
+
+print('end of the file')
 
