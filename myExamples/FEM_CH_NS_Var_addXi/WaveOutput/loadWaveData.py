975d91510f6d54a26764ed77670679a7b3069692
yczhang <yoczhang@126.com>
2022-03-01 16:24:10 +0800

    update

---
+++ myExamples/FEM_CH_NS_Var_addXi/WaveOutput/loadWaveData.py
@@ -0,0 +1,19 @@
+#!/anaconda3/envs/FEALPy/bin python3.8
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: loadWaveData.py
+# @Author: Yongchao Zhang
+# @Institution: Northwest University, Xi'an, Shaanxi, China
+# @E-mail: yoczhang@126.com, yoczhang@nwu.edu.cn
+# @Site: 
+# @Time: Mar 01, 2022
+# ---
+
+
+import numpy as np
+
+wavedata = np.load('./time_position_Xi_20220227-173723.npy')
+
+
+print('end of the file')
