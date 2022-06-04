a9609411354426343970738e4113c041692a07a4
yczhang <yoczhang@126.com>
2022-06-04 22:38:07 +0800

    update

--- example_update1/HeatEquationAFEM2d_yctest.py
+++ example_update1/HeatEquationAFEM2d_yctest.py
@@ -3,8 +3,12 @@
 
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from matplotlib import rc
+from fealpy.mesh.adaptive_tools import mark
+import matplotlib  # 为了解决画图时采用 GUI (plt.show()) 的形式时, python3.8 崩溃的情况.
+matplotlib.use("TkAgg")  # 为了解决画图时采用 GUI (plt.show()) 的形式时, python3.8 崩溃的情况.
+import matplotlib.pyplot as plt
 
 rc('text', usetex=True)
 
@@ -32,40 +36,33 @@ from fealpy.tools.show import showmultirate
 from scipy.sparse.linalg import spsolve
 
 # 拷贝对象
-import copy
+# import copy
 
 ## 参数解析
 parser = argparse.ArgumentParser(description=
                                  """
                                  三角形网格自适应有限元方法求解热传导方程
                                  """)
-
 parser.add_argument('--ns',
                     default=10, type=int,
                     help='空间各个方向剖分段数， 默认剖分 10 段.')
-
 parser.add_argument('--nt',
                     default=100, type=int,
                     help='时间剖分段数，默认剖分 100 段.')
-
 parser.add_argument('--tol',
-                    default=1e-5, type=float,
-                    help='自适应加密停止阈值，默认设定为 0.05.')
-
+                    default=5e-3, type=float,
+                    help='自适应加密停止阈值，默认设定为 0.01.')
 parser.add_argument('--rtheta',
-                    default=0.7, type=float,
-                    help='自适应加密参数，默认设定为 0.7.')
-
+                    default=0.4, type=float,
+                    help='自适应加密参数，默认设定为 0.3.')
 parser.add_argument('--ctheta',
-                    default=0.5, type=float,
+                    default=0.2, type=float,
                     help='自适应粗化参数，默认设定为 0.3.')
 
 args = parser.parse_args()
-
 ns = args.ns
 nt = args.nt
 tol = args.tol
-
 rtheta = args.rtheta
 ctheta = args.ctheta
 
@@ -74,46 +71,25 @@ domain = pde.domain()
 c = pde.diffusionCoefficient
 
 tmesh = UniformTimeLine(0, 1, nt)  # 均匀时间剖分
-
 smesh = MF.boxmesh2d(domain, nx=ns, ny=ns, meshtype='tri')
+smesh.add_plot(plt)
+# plt.show()
+plt.savefig('./test-' + str(0) + '.png')
+plt.close()
 
-# smesh.add_plot(plt)
-# plt.savefig('./test-' + str(0) + '.png')
-# plt.close()
 i = 0
-# while True:
-#     # 初始网格的自适应
-#     space = LagrangeFiniteElementSpace(smesh, p=1)  # 构造线性元空间
-#     # 当前时间步的有限元解
-#     uh0 = space.interpolation(pde.init_value)
-#     eta = space.recovery_estimate(uh0, method='area_harmonic')
-#     err = np.sqrt(np.sum(eta ** 2))
-#     if err < tol:
-#         break
-#     isMarkedCell = smesh.refine_marker(eta, rtheta, method='L2')
-#
-#     # |--- test
-#     # isMarkedCell[smesh.ds.cellstart:] = True
-#
-#     smesh.refine_triangle_rg(isMarkedCell)
-#     i += 1
-#     smesh.add_plot(plt)
-#     plt.savefig('./test-' + str(i + 1) + '.png')
-#     plt.close()
-
 p = 2
 space = LagrangeFiniteElementSpace(smesh, p=p)
 uh0 = space.interpolation(pde.init_value)
 
 for j in range(0, nt):
-
     # 下一个的时间层 t1
     t1 = tmesh.next_time_level()
     print("t1=", t1)
 
     while True:
         # 下一层时间步的有限元解
-        uh1 = space.function()
+        uh = space.function()
         A = c * space.stiff_matrix()  # 刚度矩阵
         M = space.mass_matrix()  # 质量矩阵
         dt = tmesh.current_time_step_length()  # 时间步长
@@ -134,73 +110,66 @@ for j in range(0, nt):
             return pde.dirichlet(p, t1)
 
         bc = DirichletBC(space, dirichlet)
-        GD, F = bc.apply(G, F, uh1)
+        GD, F = bc.apply(G, F, uh)
 
         # 代数系统求解
-        uh1[:] = spsolve(GD, F)
-        eta = space.recovery_estimate(uh1, method='area_harmonic')
+        uh[:] = spsolve(GD, F)
+        eta = space.recovery_estimate(uh, method='area_harmonic')
         err = np.sqrt(np.sum(eta ** 2))
         print('errrefine', err)
         if err < tol:
             break
         else:
-            # 加密并插值
-            NN0 = smesh.number_of_nodes()
-            Ncell = smesh.number_of_cells()
-            edge = smesh.entity('edge')
-            # isMarkedCell = smesh.refine_marker(eta, rtheta, method='L2')
-            # options = smesh.adaptive_options(method='max', theta=0.7)
-            isMarkedCell = np.zeros((Ncell,), dtype=bool)
-            isMarkedCell[0:5] = True
+            isMarkedCell = mark(eta, theta=rtheta)
             c2d = space.cell_to_dof()
-            options = smesh.bisect_options(data={'uh': uh1[c2d]})
+            options = smesh.bisect_options(data={'uh': uh[c2d]})
             smesh.bisect(isMarkedCell=isMarkedCell, options=options)
-            # isMarkedCell = smesh.adaptive(eta, options)
-            # smesh.refine_triangle_rg(isMarkedCell)
             i += 1
-            # smesh.add_plot(plt)
-            # plt.savefig('./test-' + str(i + 1) + '.png')
-            # plt.close()
+
+            smesh.add_plot(plt)
+            # plt.show()
+            plt.savefig('./test-' + str(i + 1) + '.png')
+            plt.close()
+
             space = LagrangeFiniteElementSpace(smesh, p=p)
             print('refinedof', space.number_of_global_dofs())
             thedof, indices = np.unique(space.cell_to_dof().flatten(), return_index=True)
-            uh1_ = options['data']['uh'].flatten()
+            uh_matrix = options['data']['uh'].flatten()
             uh0 = space.function()
-            uh0[:] = uh1_[indices]
+            uh0[:] = uh_matrix[indices]
 
     # 粗化网格并插值
-    isMarkedCell = smesh.refine_marker(eta, ctheta, 'COARSEN')
-    smesh.coarsen_triangle_rg(isMarkedCell)
+    isMarkedCell = mark(eta, theta=ctheta, method='COARSEN')
+    c2d = space.cell_to_dof()
+    options = {'data': {'uh': uh[c2d]}}
+    smesh.coarsen(isMarkedCell, options=options)
     i += 1
-    # smesh.add_plot(plt)
-    # plt.savefig('./test-' + str(i + 1) + '.png')
-    # plt.close()
-    space = LagrangeFiniteElementSpace(smesh, p=1)
-    print('coarsendof', space.number_of_global_dofs())
-    uh2 = space.function()
-    retain = smesh.retainnode
-    uh2[:] = uh1[retain]
-    uh1 = space.function()
-    uh0 = space.function()
-    uh1[:] = uh2
 
+    # plt.clf()
+    smesh.add_plot(plt)
+    # plt.show()
+    plt.savefig('./test-' + str(i + 1) + '.png')
+    plt.close()
+    space = LagrangeFiniteElementSpace(smesh, p=p)
+    print('coarsendof', space.number_of_global_dofs())
+    thedof, indices = np.unique(space.cell_to_dof().flatten(), return_index=True)
+    uh_matrix = options['data']['uh'].flatten()
+    uh0 = space.function()
+    uh0[:] = uh_matrix[indices]
 
     # t1 时间层的误差
     @cartesian
     def solution(p):
         return pde.solution(p, t1)
 
-
-    error = space.integralalg.error(solution, uh1)
+    error = space.integralalg.error(solution, uh0)
     print("error:", error)
 
     # 画数值解图像
     if (t1 == 0.01) | (t1 == 0.49) | (t1 == 0.99):
         fig = plt.figure()
         axes = fig.add_subplot(1, 1, 1, projection='3d')
-        uh1.add_plot(axes, cmap='rainbow')
-    uh0[:] = uh1
-    uh1[:] = 0.0
+        uh0.add_plot(axes, cmap='rainbow')
 
     # 时间步进一层 
     tmesh.advance()
