54a378920bba989f2accd9c6332f7690d87aa08e
yczhang <yoczhang@126.com>
2022-06-09 22:49:50 +0800

    update

--- example_update1/HeatEquationAFEM2d_yctest.py
+++ example_update1/HeatEquationAFEM2d_yctest.py
@@ -73,7 +73,7 @@ c = pde.diffusionCoefficient
 tmesh = UniformTimeLine(0, 1, nt)  # 均匀时间剖分
 smesh = MF.boxmesh2d(domain, nx=ns, ny=ns, meshtype='tri')
 smesh.add_plot(plt)
-# plt.show()
+plt.show()
 plt.savefig('./test-' + str(0) + '.png')
 plt.close()
 
@@ -142,7 +142,7 @@ for j in range(0, nt):
     isMarkedCell = mark(eta, theta=ctheta, method='COARSEN')
     c2d = space.cell_to_dof()
     options = {'data': {'uh': uh[c2d]}}
-    smesh.coarsen(isMarkedCell, options=options)
+    # smesh.coarsen(isMarkedCell, options=options)
     i += 1
 
     # plt.clf()
